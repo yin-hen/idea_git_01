12a5c6038ec83058c7dd47fdb3863ef06624e2a0
yinhen <841681032@qq.com>
2023-04-29 18:26:07 +0800

    hot-fix commit 2

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
         System.out.println("hello git3");
+        System.out.println("hot-fix test");
     }
 }
